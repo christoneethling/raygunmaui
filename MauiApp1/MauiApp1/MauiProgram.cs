37d0e67245b12f9e56fc1766e34997bc5962dda0
Christo Neethling <christo@mastech.co.za>
2023-06-13 20:17:48 +0200

    Initial sample of MAUI Blazor app using Raygun

---
+++ MauiApp1/MauiApp1/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using MauiApp1.Data;
+using Raygun4Maui;
+
+namespace MauiApp1;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			})
+            .AddRaygun4Maui("MPtgGj55U6rh69hY1olHA");
+
+		builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+        builder.Services.AddBlazorWebViewDeveloperTools();
+		builder.Logging.AddDebug();
+#endif
+
+		builder.Services.AddSingleton<WeatherForecastService>();
+
+		return builder.Build();
+	}
+}
